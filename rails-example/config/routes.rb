dfd9b626a8f5358aabbb91917539d69e86c2453b
ricardo.araujo <ricardo.araujo@olx.com>
2022-06-20 17:11:45 +0100

    added user Model

--- rails-example/config/routes.rb
+++ rails-example/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "exercises#index"
   resources :exercises do
     resources :answers 
     resources :user_answers #, only: [ :new, :create ]
--- rails-example/db/schema.rb
+++ rails-example/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_19_101137) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_20_155122) do
   create_table "answers", force: :cascade do |t|
     t.text "answer"
     t.boolean "true_answer"
@@ -35,6 +35,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_19_101137) do
     t.datetime "updated_at", null: false
     t.index ["answer_id"], name: "index_user_answers_on_answer_id"
     t.index ["exercise_id"], name: "index_user_answers_on_exercise_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "exercises"
